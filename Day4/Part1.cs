a8239d5dac1517773fc7e2caafd24f435db3efe7
Inmo Yang <inmo.yang@fnz.com>
2022-10-11 09:53:31 +1300

    Completed Day 3

---
+++ Day4/Part1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualBasic.FileIO;
+
+public class lifeSupportRating
+{
+    public static void Main()
+    {
+        string filename = @"C:\Users\inmo.yang\source\repos\AdventOfCode\Day4\input.txt";
+        var inputArray = File.ReadAllLines(filename);
+        var input = new List<string>(inputArray);
+
+
+    }
+}
